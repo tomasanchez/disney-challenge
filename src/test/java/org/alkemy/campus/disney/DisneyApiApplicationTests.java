5bfde3da339ae5a43680c8fa88e538fa9a51b4a3
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-01 14:47:39 -0300

    build: project initialized with Spring Initializr

---
+++ src/test/java/org/alkemy/campus/disney/DisneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alkemy.campus.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
