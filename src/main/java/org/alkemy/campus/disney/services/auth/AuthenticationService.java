df0c6d3337731d0e7e7762b9d49297a4070e2976
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-02 17:37:54 -0300

    docs(auth): service interface

--- src/main/java/org/alkemy/campus/disney/services/auth/AuthenticationService.java
+++ src/main/java/org/alkemy/campus/disney/services/auth/AuthenticationService.java
@@ -4,5 +4,14 @@ import org.alkemy.campus.disney.auth.DUser;
 import org.alkemy.campus.disney.exceptions.auth.UserAlreadyExistsException;
 
 public interface AuthenticationService {
+
+  /**
+   * Register a new user in the database with the role USER.
+   * 
+   * @param user the user data to be registered
+   * @return the registered user
+   * @throws UserAlreadyExistsException
+   */
   public DUser registerUser(DUser user) throws UserAlreadyExistsException;
+
 }
