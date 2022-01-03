a073f7309f8851625dfadf718019d1ee2bad3145
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-01-03 19:13:20 -0300

    refactor: change to identity column for IDs

--- src/main/java/org/alkemy/campus/disney/core/db/PersitentEntity.java
+++ src/main/java/org/alkemy/campus/disney/core/db/PersitentEntity.java
@@ -25,7 +25,7 @@ public abstract class PersitentEntity implements Serializable {
   // --------------------------------------------------------------------------------------------
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   // --------------------------------------------------------------------------------------------
